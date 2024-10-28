51404884b9e48be3ad59cd0401906b12785c27ac
kadir-zeren <k4dzeren@gmail.com>
2024-10-28 13:24:13 +0300

    practice

--- python_03.py
+++ python_03.py
@@ -5,3 +5,255 @@
 11 // 2
 5 ** 3 
 10 % 3 
+
+3 + 5
+"3" + "5"
+3 * 5
+"abc" * 3
+"3" + 'abc'
+
+print(11-7)
+print(4 + 11.0)
+print('11 - 7')
+# print('4' + 4) 
+
+print('clarusway')
+print('3+5')
+print(' abc' )
+print('!-** asdflkasdjlfk')
+
+num1, num2 = 81, 55
+num3 = num1 - num2
+print(num3)
+
+a,b = 3, 5
+print(a,b) 
+
+r = 5
+pi = 3.14
+area = pi * r ** 2
+print(area)
+
+r = 5
+pi = 3.14
+area = pi * (r ** 2)
+print(area)
+
+print(11 % 2) # remainder of this division is 1
+# it means 11 is an odd number
+print((4 * 5) / 2) # parentheses are used as in normal math operations
+
+print(2 ** 3) # 2 to the power of 3
+print(3 ** 2) # square of 3
+a = 2
+b = 8
+print((a * b) ** 0.5) # square root
+
+5 ** 0.5
+
+16 ** (1/2)
+
+a = 5
+
+a = a + 1
+
+a +=1
+
+a
+
+A = 10
+
+A = A * 2
+
+A
+
+A *= 2
+
+A
+
+3 + 5 * 2
+
+a = (1 + 1 ) ** (4 ** (2 / 2 * 2) / 2)
+print(a)
+
+a = - 3 ** 2
+print(a)
+
+a = (-3) ** 2
+print(a)
+
+a = 3
+b = 4
+c = (a ** 2 + b ** 2) ** 0.5
+print(c)
+
+a = 3
+b = 4
+hipotenus = (a ** 2 + b ** 2) ** 0.5
+print(hipotenus)
+
+print(a,b,hipotenus)
+print(a,b,hipotenus, sep=' ----- >')
+
+print(a)
+print(b)
+print(hipotenus)
+
+print(a, end="bitiiiiiiiii" )
+print(b)
+print(hipotenus)
+
+bir = 'ilk satir'
+iki = 'ikinci satir'
+uc = 'ucuncu satir'
+
+print(bir, iki,uc)
+
+print(bir, iki, uc, sep=' ')
+
+print(bir, iki,uc, sep='_____bosluk_____')
+
+print(bir)
+print(iki)
+print(uc)
+
+print(bir, end = "\n")
+print(iki, end = '\n')
+print(uc)
+
+print(bir, end = "\n **** ")
+print(iki, end = '\n')
+print(uc)
+
+print(bir, iki, uc, sep='* - * ', end = '&' )
+print(bir, iki, uc, sep=' ')
+print(bir, iki, uc, sep=' ')
+
+print (bir, iki, uc, sep='* - * ', end = '&\n' )
+print(bir, iki,uc, sep=' ')
+print(bir, iki, uc, sep=' ')
+
+number = 2021
+text = "we have reached"
+print(text, number)
+
+text1 = "I bought"
+text2 = "kg. of apple this morning"
+amount = 6
+text3 = text1 + " " + str(amount) + " " + text2
+print(text1, amount, text2)
+print("I bought", 6, "kg. of apple this morning")
+print("I bought " + "6 " + "kg. of apple this morning")
+print(text3)
+
+print('abc', 555)
+
+text1 = "I bought"
+text3 = text1 + 'apple'
+
+text3
+
+print ('C:\\north pole\noise_penguins. txt' )
+print('--------')
+print('first', 'second', 'third', sep='\t')
+
+print ('we are', '\boosting', 'our' , '\brotherhood' )
+print('it\'s essential to learn Python\'s libraries in IT World')
+
+text = 'Bu\gun\ben\neden\taksiye\bindim'
+print(text)
+
+print('Bu\gun\\ben\\neden\\taksiye\\bindim')
+
+print(r'Bu\gun\ben\neden\taksiye\bindim')
+
+'i\'m great'
+
+"i'm great"
+
+True and True
+True and False
+False and False
+False and True
+True or True
+True or False
+False or False
+False or True
+
+False and True and True and True
+
+True or False or False
+
+bool_var = True and not True
+print(bool_var)
+
+bool(False)
+ 
+bool(0)
+
+bool([])
+
+bool ('')
+
+1 and 1
+
+1 and 0
+
+bool('abcdeg')
+
+bool(5)
+
+bool (None)
+
+not True
+
+not False
+
+not 'string'
+
+True and False or not False or False 
+
+True and True and True
+
+'string' and 5 and 6.2
+
+5 and 0 and False and 'abcc'
+
+False or False or False
+
+False or False or False or True
+
+False or '' or 'False' or '35' or 45
+
+print(2 and 3)
+
+print(1 and 0)
+
+print(2 and "hello world")
+print([] and "be happy!")
+print(None and ())
+
+print(2 or 3)
+print(None or 1)
+
+print(2 or "hello world")
+print([] or "be happy!")
+print(None or ())
+print({} or 0)
+print({0} or False)
+
+best ='Clarusway'
+best[2]
+best [2:]
+
+fruit = 'Orange'
+print('Word:' , fruit)
+print('First letter:', fruit[0])
+print('Second letter:', fruit[1])
+print("3rd to 5th letters:", fruit[2:5])
+print("Letter all after 3rd:", fruit[2:])
+
+text = 'abcdef'
+text [0]
+text[1]
+text [5]
